482af72a701234347eee2947d7a8cd82c07690cf
Guy Blanc <gblanc@stanford.edu>
2016-07-09 17:38:00 -0700

    Added stripping

---
+++ TextToApp/processing.py
@@ -0,0 +1,35 @@
+import json
+import nltk
+
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+import sys
+import subprocess
+
+
+# sentence= 'i want to look up five guys on yelp, oh my phone can'
+sentence = "oh going to san diego. i wonder what the weather is like"
+
+
+keep = set(['CD', 'NN', 'NNP', 'NNPS', 'NNS'])
+
+
+text = sentence.split()  # word_tokenize(sentence)
+parts = pos_tag(text)
+print(parts)
+text = list(map(lambda x: x[0].upper() + x[1:], text))
+parts = pos_tag(text)
+
+def stripExceptNouns(sentence):
+    upper = list(map(lambda x: x[0].upper() + x[1:], sentence.split()))
+    parts = pos_tag(upper)
+    kept = []
+    for part in parts:
+        if part[1] in keep:
+            kept.append(part)
+    return kept
+    
+
+print(parts)
+print()
+print(stripExceptNouns(sentence))
